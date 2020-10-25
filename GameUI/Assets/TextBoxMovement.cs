f49388e37f960116e29544ff4345fff3a620e4e7
d4nl31010cv <35624892+Daniel10106@users.noreply.github.com>
2020-10-25 03:16:50 +0200

    Helping Hand
    UI + prefabs

---
+++ GameUI/Assets/TextBoxMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 5000f;
+    }
+
+    private void FixedUpdate()
+    {
+             
+    }
+}
